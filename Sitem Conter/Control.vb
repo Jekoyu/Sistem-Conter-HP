0cbf33da25af5013d3684bc69545f4bf0f330d59
Jekoyu <jekoyu157@gmail.com>
2021-03-22 10:00:42 +0700

    Add project files.

---
+++ Sitem Conter/Control.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Data.SqlClient
+Module Control
+    Public conn As SqlConnection
+    Public cmd As SqlCommand
+    Public dr As SqlDataReader
+    Public da As New SqlDataAdapter
+    Public ds As DataSet
+    Public role As String
+    Public m As String
+    Public q As String
+    Public clm As String
+    Public tbl As String
+    Public Sub Msg(ByVal m As String)
+        MessageBox.Show(m)
+    End Sub
+    Public Sub Koneksi()
+        Try
+            conn = New SqlConnection("Data Source=DESKTOP-VOQEHKC\SQLEXPRESS;Initial Catalog=db_conter;Integrated Security=True;MultipleActiveResultSets=True")
+            conn.Open()
+        Catch ex As Exception
+            m = ex.ToString
+            Msg(m)
+        End Try
+    End Sub
+
+    Function Tampil(ByVal tbl As String, clm As String) As Boolean
+        Try
+            da = New SqlDataAdapter("select " + clm + " from " + tbl, conn)
+            ds = New DataSet
+            da.Fill(ds)
+        Catch ex As Exception
+            m = ex.ToString
+            Msg(m)
+        End Try
+        Return True
+    End Function
+    Function Cari(ByVal q As String) As Boolean
+        Try
+            cmd = New SqlCommand(q, conn)
+            dr = cmd.ExecuteReader
+            dr.Read()
+        Catch ex As Exception
+            m = ex.ToString
+            Msg(m)
+        End Try
+        Return True
+    End Function
+    Function ENQ(ByVal q As String) As Boolean
+        Try
+            cmd = New SqlCommand(q, conn)
+            cmd.ExecuteNonQuery()
+        Catch ex As Exception
+            m = ex.ToString
+            Msg(m)
+        End Try
+        Return True
+    End Function
+
+End Module
